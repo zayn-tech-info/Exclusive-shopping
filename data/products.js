1864c871c90acc4dc073334953d1d7b20388923c
zayn-tech-info <zayntech.info@gmail.com>
2025-04-29 15:15:49 +0100

    Separated each components to their files

---
+++ data/products.js
@@ -0,0 +1,16 @@
+// Get all the products needed from the api
+const url = "https://dummyjson.com/products";
+export const getProducts = async () => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    const products = data.products;
+    console.log(products);
+    return products;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+};
+getProducts();
+
+
---
+++ scripts/exclusive.js
@@ -0,0 +1,83 @@
+import { cart } from "../data/cart.js";
+import { getProducts } from "../data/products.js";
+const productsContainer = document.querySelector(".products");
+const renderProducts = async () => {
+  const products = await getProducts();
+  console.log(products);
+
+  let productsHtml = "";
+  products.forEach((product) => {
+    productsHtml += `
+	<div class="product-item">
+	  <div class="bg-gray-100 group">
+		<div class="relative pt-10 px-10 mx-auto">
+		  <div>
+			<div class="h-43 flex justify-center items-center">
+			  <img class="p-3" src="${product.images}" alt="${product.title}" />
+			</div>
+			<div class="absolute text-black right-3 flex text-xl space-y-5 flex-col top-3">
+			  <ion-icon class="bg-white rounded-full p-1" name="eye-outline"></ion-icon>
+			  <ion-icon class="bg-white rounded-full p-1" name="heart-outline"></ion-icon>
+			</div>
+		  </div>
+		</div>
+		<button
+		  class="addToCart flex justify-center opacity-0 rounded-b-md bg-black w-full text-white py-2 group-hover:opacity-100 transition duration-300 ease-in-out"
+		  data-product-id="${product.id}"
+		>
+		  Add To Cart
+		</button>
+	  </div>
+	  <div class="Product-detail mt-2 space-y-1">
+		<p class="font-medium">${product.title}</p>
+		<div class="flex gap-5">
+		  <span class="font-medium text-[#DB4444]">$${product.discountPercentage}</span>
+		  <span class="line-through font-medium text-gray-500">$${product.price}</span>
+		</div>
+		<div class="flex items-center gap-5">
+		  <img src="/Images/Products/rate-4.5-star-75.png" alt="${product.title}" />
+		  <p class="text-yellow-600 font-medium">${product.rating}</p>
+		</div>
+	  </div>
+	</div>`;
+    productsContainer.innerHTML = productsHtml;
+
+    const addToCartbtns = document.querySelectorAll(".addToCart");
+    addToCartbtns.forEach((button) => {
+      button.addEventListener("click", () => {
+        const productId = button.dataset.productId;
+
+        let matchingItem;
+        cart.forEach((cartItem) => {
+          if (cartItem.productId === productId) {
+            matchingItem = cartItem;
+          }
+        });
+
+        if (matchingItem) {
+          matchingItem.quantity++;
+        } else {
+          cart.push({
+            productId: productId,
+            quantity: 1,
+          });
+        }
+        console.log(cart);
+
+        let cartQuantity = 0;
+        const cartTotal = document.querySelector(".js-cart-quantity");
+        cart.forEach((cartItem) => {
+          cartQuantity += cartItem.quantity;
+          cartTotal.innerText = cartQuantity;
+          console.log(cartQuantity);
+        });
+      });
+    });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderProducts();
+});
+
+console.log(cart);
