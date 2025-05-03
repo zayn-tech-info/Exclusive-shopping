7ff201f322103969fff143d0a3bee5a5c3c49666
zayn-tech-info <zayntech.info@gmail.com>
2025-05-03 10:10:27 +0100

    Add items to checkout page

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart} from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { getProducts } from "../data/products.js";
 
 let matchingProduct;
@@ -11,8 +11,33 @@ const renderCart = async () => {
     products.forEach((product) => {
       if (product.id === productId) {
         matchingProduct = product;
-        cartSummaryHtml += `
-        <div class="mx-auto my-10 shadow py-2 cart-item-container-${matchingProduct.id}">
+        cartSummaryHtml += renderCartHtml(matchingProduct, cartItem);
+        document.querySelector(".cartSummaryHtml").innerHTML = cartSummaryHtml;
+        const removeFromCartList = document.querySelectorAll(".removeFromCart");
+        console.log(removeFromCartList);
+        removeFromCartList.forEach((button) => {
+          button.addEventListener("click", () => {
+            const productId = Number(button.dataset.productId);
+            console.log(productId);
+            removeFromCart(productId);
+            const container = document.querySelector(
+              `.cart-item-container-${productId}`
+            );
+            container.remove();
+          });
+        });
+      }
+    });
+  });
+
+
+};
+
+function renderCartHtml(matchingProduct, cartItem) {
+  return `
+        <div class="mx-auto my-10 shadow py-2 cart-item-container-${
+          matchingProduct.id
+        }">
           <div class="grid grid-cols-4 items-center">
             <div class="mx-auto flex gap-5">
               <img class="w-10" src="${matchingProduct.images}" alt="" />
@@ -23,8 +48,8 @@ const renderCart = async () => {
             </div>
             <div class="mx-auto">
               <input class="text-black w-10 border-1 outline-0 text-xl font-medium pl-4" type="text" placeholder="${
-          cartItem.quantity
-        }"/>
+                cartItem.quantity
+              }"/>
             </div>
             <div class="mx-auto flex items-center gap-20">
               <p>${matchingProduct.discountPercentage * cartItem.quantity}</p>
@@ -32,27 +57,14 @@ const renderCart = async () => {
               <span>
                 <ion-icon 
                   data-product-id="${matchingProduct.id}" 
-                  class="removeFromCart ${matchingProduct.id} text-4xl cursor-pointer hover:bg-[#DB4444] rounded-full hover:text-white duration-200 transition ease-in-out" name="close-circle-outline"></ion-icon>
+                  class="removeFromCart ${
+                    matchingProduct.id
+                  } text-4xl cursor-pointer hover:bg-[#DB4444] rounded-full hover:text-white duration-200 transition ease-in-out" name="close-circle-outline"></ion-icon>
               </span> 
             </div>
           </div>
         </div>`;
-        document.querySelector(".cartSummaryHtml").innerHTML = cartSummaryHtml;
-        const removeFromCartList = document.querySelectorAll(".removeFromCart");
-        console.log(removeFromCartList);
-        removeFromCartList.forEach((button) => {
-          button.addEventListener("click", () => {
-            const productId = Number(button.dataset.productId);
-            console.log(productId);
-            removeFromCart(productId)
-            const container =  document.querySelector(`.cart-item-container-${productId}`)
-            container.remove()
-          });
-        });
-      }
-    });
-  });
-};
+}
 renderCart();
 console.log(cart);
 console.log(cartSummaryHtml);
---
+++ scripts/checkout.js
@@ -0,0 +1,40 @@
+import { cart } from "../data/cart.js";
+import { getProducts } from "../data/products.js";
+let chechOutSummaryHtml = "";
+
+let matchingProduct;
+let cartSummaryHtml = "";
+const renderCart = async () => {
+  const products = await getProducts();
+  console.log(products);
+  cart.forEach((cartItem) => {
+    const productId = cartItem.productId;
+    products.forEach((product) => {
+      if (product.id === productId) {
+        matchingProduct = product;
+        chechOutSummaryHtml += renderCheckOutHtml(matchingProduct, cartItem);
+        document.querySelector(".checkoutSummaryHtml").innerHTML =
+          chechOutSummaryHtml;
+      }
+    });
+  });
+};
+
+function renderCheckOutHtml(matchingProduct, cartItem) {
+  return `
+	<div class="flex my-10 justify-between items-center">
+		<div class="flex gap-10 items-center">
+			<img
+			class="w-10"
+			src="${matchingProduct.images}"
+			alt=""
+			/>
+			<p>${matchingProduct.title}</p>
+		</div>
+		<p>${matchingProduct.discountPercentage * cartItem.quantity}</p>
+		
+	</div>`;
+}
+renderCart();
+console.log(cart);
+console.log(cartSummaryHtml);
